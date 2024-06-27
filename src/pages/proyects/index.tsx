0476c65e75da6513d32acc7fbb0e8c5bcadda2cf
josesantana.web <josesantana.web@gmail.com>
2024-06-27 16:35:05 -0400

    refactor: Update project descriptions in ProjectsPage component

--- src/pages/proyects/index.tsx
+++ src/pages/proyects/index.tsx
@@ -2,42 +2,42 @@ const DATA = [
   {
     image: 'project-01.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
   {
     image: 'project-02.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
   {
     image: 'project-03.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
   {
     image: 'project-04.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
   {
     image: 'project-01.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
   {
     image: 'project-02.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
   {
     image: 'project-03.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
   {
     image: 'project-04.png',
     date: 'Bogotá 30/04/24',
-    deescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam ',
   },
 ]
 
@@ -46,15 +46,15 @@ const ProjectsPage = () => {
     <main>
       <div className="flex justify-center py-20">
         <div className="container">
-          <h4 className="mb-20 text-5xl font-medium text-black">APLICACIONES</h4>
+          <h4 className="mb-20 text-[83px] font-[900] text-black border-b border-dashed border-black">PROYECTOS</h4>
           <div className="grid items-center grid-cols-4 gap-12">
             {DATA.map((item, index) => (
               <div className="flex flex-col" key={index}>
                 <img src={`/images/${item.image}`} alt="app" className="w-full h-[200px] mb-3" />
-                <h4 className="mb-3 text-base font-semibold">{item.date}</h4>
-                <p className="mb-3 text-base text-black">{item.deescription}</p>
+                <h4 className="mb-3 text-[18px] font-[700]">{item.date}</h4>
+                <p className="mb-3 text-[24px] text-black font-[700] leading-[26px]">{item.description}</p>
                 <div className="flex justify-start">
-                  <a href="" className="text-base font-medium text-black underline">
+                  <a href="" className="text-base font-[700] text-black">
                     Ver mas
                   </a>
                 </div>
