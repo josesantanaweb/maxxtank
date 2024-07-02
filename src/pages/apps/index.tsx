9eb879d45cc4f8731b11ef8341946db953c31a13
josesantana.web <josesantana.web@gmail.com>
2024-07-01 23:18:31 -0400

    feat: Add Components section to AppsPage

--- src/pages/apps/index.tsx
+++ src/pages/apps/index.tsx
@@ -1,5 +1,6 @@
 import Advantages from '@/components/Apps/Advantages'
 import Apps from '@/components/Apps/Apps'
+import Components from '@/components/Apps/Components'
 import Diameters from '@/components/Apps/Diameters'
 import Storage from '@/components/Apps/Storage'
 import StorageNetwork from '@/components/Apps/StorageNetwork'
@@ -10,10 +11,12 @@ const AppsPage = () => {
     <main>
       <Apps />
       <Storage />
+      <Components />
       <Tanks />
       <Diameters />
       <Advantages />
       <StorageNetwork />
+      <Components />
       <Diameters />
       <Advantages />
     </main>
