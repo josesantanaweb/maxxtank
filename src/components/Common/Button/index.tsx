5e45d9571ea7321f0e8aafbe6b12f68c7905ad18
josesantana.web <josesantana.web@gmail.com>
2024-08-25 13:43:37 -0400

    fix: contact

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -6,6 +6,7 @@ interface ButtonProps {
 const Button = ({ children, onClick }: ButtonProps) => {
   return (
     <button
+      type="button"
       onClick={onClick}
       className="flex items-center justify-center gap-1 px-3 py-2 text-sm text-white rounded-lg bg-gradient-to-r from-primary-100 to-primary-200"
     >
--- src/components/Common/Contact/index.tsx
+++ src/components/Common/Contact/index.tsx
@@ -1,9 +1,27 @@
 import Button from '@/components/Common/Button'
+import { useState } from 'react'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 const Contact = ({ setOpen, title }: any) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [company, setCompany] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+
+  const mailto = `mailto:contacto@maxxtank.com?cc=${encodeURIComponent(email)}&subject=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(message)}%0ANombre%3A ${encodeURIComponent(firstName)} ${encodeURIComponent(
+    lastName
+  )}%0ACompania%3A ${encodeURIComponent(company)}%0ATel%C3%A9fono%3A ${encodeURIComponent(phone)}`
+
   return (
     <div className="fixed top-0 z-10 flex items-center justify-center w-full h-full">
+      <a href="mailto:email@example.com?subject=Asunto&body=Mensaje%20del%20cuerpo%20del%20correo%20electr%C3%B3nico%0ATel%C3%A9fono%3A%201234567890">
+        Enviar correo
+      </a>
       <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-35" onClick={() => setOpen(false)}></div>
       <div className="flex bg-gray-100 rounded-xl w-[600px] relative z-10 py-6 flex-col">
         <div className="flex justify-end w-full px-5">
@@ -19,6 +37,7 @@ const Contact = ({ setOpen, title }: any) => {
               <div className="w-1/2">
                 <input
                   type="text"
+                  onChange={(e) => setFirstName(e.target.value)}
                   placeholder="Nombre(s)"
                   className="w-full px-4 py-2 bg-gray-100 border border-black focus:outline-none focus:ring-2 focus:ring-blue-400 placeholder:text-black"
                 />
@@ -26,6 +45,7 @@ const Contact = ({ setOpen, title }: any) => {
               <div className="w-1/2">
                 <input
                   type="text"
+                  onChange={(e) => setLastName(e.target.value)}
                   placeholder="Apellido(s)"
                   className="w-full px-4 py-2 bg-gray-100 border border-black placeholder:text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
                 />
@@ -34,6 +54,7 @@ const Contact = ({ setOpen, title }: any) => {
             <div>
               <input
                 type="email"
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Correo electrónico"
                 className="w-full px-4 py-2 bg-gray-100 border border-black placeholder:text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
               />
@@ -42,6 +63,7 @@ const Contact = ({ setOpen, title }: any) => {
               <div className="w-1/2">
                 <input
                   type="text"
+                  onChange={(e) => setPhone(e.target.value)}
                   placeholder="Número de teléfono"
                   className="w-full px-4 py-2 bg-gray-100 border border-black placeholder:text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
                 />
@@ -49,6 +71,7 @@ const Contact = ({ setOpen, title }: any) => {
               <div className="w-1/2">
                 <input
                   type="text"
+                  onChange={(e) => setCompany(e.target.value)}
                   placeholder="Compañía"
                   className="w-full px-4 py-2 bg-gray-100 border border-black placeholder:text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
                 />
@@ -57,6 +80,7 @@ const Contact = ({ setOpen, title }: any) => {
             <div>
               <input
                 type="text"
+                onChange={(e) => setSubject(e.target.value)}
                 placeholder="Asunto"
                 className="w-full px-4 py-2 bg-gray-100 border border-black placeholder:text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
               />
@@ -64,11 +88,14 @@ const Contact = ({ setOpen, title }: any) => {
             <div>
               <textarea
                 placeholder="Mensaje"
+                onChange={(e) => setMessage(e.target.value)}
                 className="w-full px-4 py-2 bg-gray-100 border border-black placeholder:text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
                 rows={4}
               ></textarea>
             </div>
-            <Button>Enviar</Button>
+            <a href={mailto}>
+              <Button>Enviar</Button>
+            </a>
           </form>
         </div>
       </div>
