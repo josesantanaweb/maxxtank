d6d8dec1b210e4524dc3ea66ee058a8e2064fcb1
josesantana.web <josesantana.web@gmail.com>
2024-09-17 14:39:11 -0400

    feat: page out

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,28 @@
-import { AppsPage, HomePage, ProjectsPage, ContactPage, TanksPage, ProjectDetailsPage } from '@/pages'
 
 export const routes = [
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/aplicaciones',
-    element: <AppsPage />,
-  },
-  {
-    path: '/proyectos',
-    element: <ProjectsPage />,
-  },
-  {
-    path: '/contacto',
-    element: <ContactPage />,
-  },
-  {
-    path: '/tanques',
-    element: <TanksPage />,
-  },
-  {
-    path: '/detalles',
-    element: <ProjectDetailsPage />,
-  },
+  // {
+  //   path: '/',
+  //   element: <HomePage />,
+  // },
+  // {
+  //   path: '/aplicaciones',
+  //   element: <AppsPage />,
+  // },
+  // {
+  //   path: '/proyectos',
+  //   element: <ProjectsPage />,
+  // },
+  // {
+  //   path: '/contacto',
+  //   element: <ContactPage />,
+  // },
+  // {
+  //   path: '/tanques',
+  //   element: <TanksPage />,
+  // },
+  // {
+  //   path: '/detalles',
+  //   element: <ProjectDetailsPage />,
+  // },
 
 ]
