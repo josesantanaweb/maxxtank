007855d486439d0af4a6d9094930372685ae4ca7
josesantana.web <josesantana.web@gmail.com>
2024-09-24 20:49:02 -0400

    remove page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import Header from '@/components/Common/Header'
-import { Suspense } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { AppRouter } from './routes/Router'
+// import Header from '@/components/Common/Header'
+// import { Suspense } from 'react'
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import { AppRouter } from './routes/Router'
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Header />
-        <Suspense>
-          <AppRouter />
-        </Suspense>
-      </Router>
-    </div>
-  )
+  return <div>server</div>
 }
 
 export default App
