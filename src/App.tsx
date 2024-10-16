94405126aa787b7e20378d9573276f1223d41a26
jetrix-quantumlabx <jetrix@quantumlabx.io>
2024-10-16 09:17:57 -0400

    Merge branch 'main' of https://github.com/josesantanaweb/maxxtank

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Header from '@/components/Common/Header'
+import { Suspense } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { AppRouter } from './routes/Router'
+
 function App() {
-  return <div>Server Is Running</div>
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Suspense>
+          <AppRouter />
+        </Suspense>
+      </Router>
+    </div>
+  )
 }
 
 export default App
